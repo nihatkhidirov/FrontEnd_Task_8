8792fa3d919fdb5bcfac01bb8cf612021c499281
nihatkhidirov <nihatmkh@code.edu.az>
2024-05-26 03:40:49 +0400

    Second Commit (Js Part Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-const input1 = document.getElementById("input1");
-const input2 = document.getElementById("input2");
-console.log(input1);
-const toplama = document.getElementById("Toplama");
-toplama.addEventListener("click", function (number1, number2) {
-  input1 = number1;
-  input2 = number2;
-  let toplam = input1 + input2;
+document.addEventListener("DOMContentLoaded", () => {
+  const input1 = document.getElementById("input1");
+  const input2 = document.getElementById("input2");
+  const result = document.getElementById("result");
+
+  document.getElementById("Toplama").addEventListener("click", () => {
+    result.value = Number(input1.value) + Number(input2.value);
+  });
+
+  document.getElementById("Cixma").addEventListener("click", () => {
+    result.value = Number(input1.value) - Number(input2.value);
+  });
+
+  document.getElementById("Bolme").addEventListener("click", () => {
+    result.value = Number(input1.value) / Number(input2.value);
+  });
+
+  document.getElementById("Vurma").addEventListener("click", () => {
+    result.value = Number(input1.value) * Number(input2.value);
+  });
 });
